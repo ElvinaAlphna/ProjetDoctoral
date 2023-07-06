36f80c5e3a975aece3e622c377d1c8c0df60e3d2
MEKUI ME KEMOE Elvina Alphna <136824120+ElvinaAlphna@users.noreply.github.com>
2023-07-05 21:39:43 -0400

    samuel foucher

--- animaloc/train/__init__.py
+++ animaloc/train/__init__.py
@@ -1,8 +1,3 @@
-from .register import LOSSES
-from .ssim import *
-from .focal import *
-from .whd import *
-from .distance import *
-from .rmse import *
-
-__all__ = ['LOSSES', *LOSSES.registry_names]
+from .trainers import *
+from .losses import *
+from .adaloss import *
